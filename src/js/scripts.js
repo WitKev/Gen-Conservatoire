ff5161f472311a871b59cabe47d64679a60704aa
WitKev <kevinwitzel24@gmail.com>
2022-12-05 16:22:32 +0100

    js

---
+++ src/js/scripts.js
@@ -0,0 +1,8 @@
+/*------------------------------------*\
+  # AFFICHAGE DU MENU POUR LE MOBILE
+\*------------------------------------*/
+
+// On cible les éléments à modifier
+const hamburger = document.querySelector(".menuBurger");
+const menu = document.querySelector(".menu");
+const page = document.documentElement;
