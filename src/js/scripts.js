560d47967fc872a2923de15eae7d1763dd705621
WitKev <kevinwitzel24@gmail.com>
2022-12-08 15:21:16 +0100

    script, chapo grid

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,3 +6,13 @@
 const hamburger = document.querySelector(".menuBurger");
 const menu = document.querySelector(".menu");
 const page = document.documentElement;
+
+// La fonction permettant de basculer l'affichage en ajoutant/supprimant des classes
+function doToggle() {
+  this.classList.toggle('menuBurger--open');
+  menu.classList.toggle('menu--open');
+  page.classList.toggle('noscroll');
+}
+
+// La fonction doToggle() est appelé lorsqu'on clique sur l'icone de menu
+hamburger.addEventListener('click', doToggle);
